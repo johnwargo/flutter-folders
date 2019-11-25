a8439e0b51159406781ca84fab71d27cf56ac3bd
John M. Wargo <johnwargo@users.noreply.github.com>
2019-11-25 15:14:32 -0500

    converted to typescript

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-//@ts-check
-#!/usr/bin / env node
-
-projectFolders: String[] = [
-  `assets`,
-  `assets/images`,
-  `assets/other`,
-  `lib/models`,
-  `lib/pages`,
-  `lib/services`,
-  `lib/utils`,
-  `lib/widgets`
+#!/usr/bin/env node
+"use strict";
+// Load the modules we'll need
+var chalk = require('chalk');
+var shell = require('shelljs');
+// Application constants
+var pubSpec = 'pubspec.yaml';
+var projectFolders = [
+    "assets",
+    "assets/images",
+    "assets/other",
+    "lib/models",
+    "lib/pages",
+    "lib/services",
+    "lib/utils",
+    "lib/widgets"
 ];
-
 // Make sure this is a Flutter project
-
-
 // Create the folders we need
-
-
 // Update the `pubspec.yaml` to point to the assets folders
-
---
+++ index.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+// Load the modules we'll need
+const chalk = require('chalk');
+var shell = require('shelljs');
+
+// Application constants
+const pubSpec = 'pubspec.yaml'
+const projectFolders: String[] = [
+  `assets`,
+  `assets/images`,
+  `assets/other`,
+  `lib/models`,
+  `lib/pages`,
+  `lib/services`,
+  `lib/utils`,
+  `lib/widgets`
+];
+
+// Make sure this is a Flutter project
+
+
+// Create the folders we need
+
+
+// Update the `pubspec.yaml` to point to the assets folders
